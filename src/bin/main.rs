9f6bdda5f495e964e98dd5d6bee746105570f63a
dahu44 <emmanuel.grandiere@laposte.net>
2025-05-31 17:34:35 +0200

    Bonjour le monde

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,16 +2,26 @@
 #![no_main]
 
 use esp_backtrace as _;
+use esp_hal::clock::CpuClock;
 use esp_hal::delay::Delay;
-use esp_hal::prelude::*;
+use esp_hal::main;
 use log::info;
 
-#[entry]
+#[main]
 fn main() -> ! {
-   esp_println::logger::init_logger_from_env();
-   let delay = Delay::new();
-   loop {
-     info!("Hello world!");
-     delay.delay(500.millis());
-   }
+    // generator version: 0.2.2
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let _peripherals = esp_hal::init(config);
+
+    esp_println::logger::init_logger_from_env();
+
+    let delay = Delay::new();
+    loop {
+        info!("Bonjour le monde !");
+        delay.delay_millis(500);
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.23.1/examples/src/bin
 }
+
